5aae94e7c23a553f22121bb32c58c4639018e8ba
Ali Krema <krema@seas.upenn.edu>
2023-02-27 20:29:34 -0500

    updated mouse.js

--- paper-js/mouse.js
+++ paper-js/mouse.js
@@ -1,19 +1,19 @@
-	// Only executed our code once the DOM is ready.
-	window.onload = function() {
-		// Get a reference to the canvas object
-		var canvas = document.getElementById('myCanvas');
-		// Create an empty project and a view for the canvas:
-		paper.setup(canvas);
-		// Create a Paper.js Path to draw a line into it:
-		var path = new paper.Path();
-		// Give the stroke a color
-		path.strokeColor = 'black';
-		var start = new paper.Point(100, 100);
-		// Move to start and draw a line from there
-		path.moveTo(start);
-		// Note that the plus operator on Point objects does not work
-		// in JavaScript. Instead, we need to call the add() function:
-		path.lineTo(start.add([ 200, -50 ]));
-		// Draw the view now:
-		paper.view.draw();
-	}
+var myPath;
+
+function onMouseDown(event) {
+	myPath = new Path();
+	myPath.strokeColor = 'black';
+}
+
+function onMouseDrag(event) {
+	myPath.add(event.point);
+}
+
+function onMouseUp(event) {
+	var myCircle = new Path.Circle({
+		center: event.point,
+		radius: 10
+	});
+	myCircle.strokeColor = 'black';
+	myCircle.fillColor = 'white';
+}
